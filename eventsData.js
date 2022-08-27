4904dbf162f18c2bcac3d3e0f118da346894265f
miguelpinto97 <78398370+miguelpinto97@users.noreply.github.com>
2022-08-27 18:20:47 -0500

    Add files via upload

---
+++ eventsData.js
@@ -0,0 +1,20 @@
+﻿var eventsData = [
+    {
+        title: 'SFF Webinar',
+        start: '2022-08-21',
+        description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
+        link: 'http://www.solutionsforfreedom.org/'
+    },
+    {
+        title: 'SFF Conference',
+        start: '2022-08-22',
+        description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
+        link: 'http://www.solutionsforfreedom.org/'
+    },
+    {
+        title: 'SFF Workshop',
+        start: '2022-08-23',
+        description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
+        link: 'http://www.solutionsforfreedom.org/'
+    }
+]
