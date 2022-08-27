3b41286f1ac477adc5b5b8b18a3348db10dac985
miguelpinto97 <78398370+miguelpinto97@users.noreply.github.com>
2022-08-27 18:35:43 -0500

    Update eventsData.js

--- eventsData.js
+++ eventsData.js
@@ -1,34 +1,36 @@
-﻿var eventsData = [
-    {
-        title: 'SFF Webinar',
-        start: '2022-08-21',
-        description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
-        link: 'http://www.solutionsforfreedom.org/'
-    },
-    {
-        title: 'SFF Conference',
-        start: '2022-08-22',
-        description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
-        link: 'http://www.solutionsforfreedom.org/'
-    },
-    {
-        title: 'SFF Workshop',
-        start: '2022-08-23',
-        description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
-        link: 'http://www.solutionsforfreedom.org/'
-    }
-]
 
- 
-             $('#calendar').fullCalendar({
-                defaultView: 'month',
-                events: eventsData,
-                eventClick: function (calEvent, jsEvent, view) {
-                    var botonHtml = "<a class='btn btn-sm btn-primary' style='width: 50%;margin: 0 auto 10px auto;display: block;' href='" + calEvent.link + "'>See more</a>"
-                    $('#event-title').text(calEvent.title);
-                    $('#event-description').html(calEvent.description);
-                    $('#event-link').html(botonHtml);
-                    $('#modal-event').modal('show')
-                    $('#modal-event').modal();
-                }
-            });
+
+
+$(document).ready(function () {
+    $('#calendar').fullCalendar({
+        defaultView: 'month',
+        events: [
+            {
+                title: 'SFF Webinar',
+                start: '2022-08-21',
+                description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
+                link: 'http://www.solutionsforfreedom.org/'
+            },
+            {
+                title: 'SFF Conference',
+                start: '2022-08-22',
+                description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
+                link:'http://www.solutionsforfreedom.org/'
+            },
+            {
+                title: 'SFF Workshop',
+                start: '2022-08-23',
+                description: 'Solutions For Freedom event in which we will talk about ... and provide valuable information on how it impacts our lives',
+                link:'http://www.solutionsforfreedom.org/'
+            }
+        ],
+        eventClick: function (calEvent, jsEvent, view) {
+            var botonHtml = "<a class='btn btn-sm btn-primary' style='width: 50%;margin: 0 auto 10px auto;display: block;' href='" + calEvent.link+"'>See more</a>"
+            $('#event-title').text(calEvent.title);
+            $('#event-description').html(calEvent.description);
+            $('#event-link').html(botonHtml);
+            $('#modal-event').modal('show')
+            $('#modal-event').modal();
+        }
+    });
+});
